43d6e32b5e1717392aae1f415a0d7e912650cad8
VictorSully79 <victorsullivan@hotmail.com>
2021-03-11 05:26:23 -0800

    1-5 Pass

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -85,9 +85,20 @@ const people = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }];
 setStatusAsAuthor(people);
 console.log(people[1].isAuthor) prints true
 ------------------------------------------------------------------------------------------------ */
+// const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
+// setStatusAsAuthor(a);
 
+// expect(a[0].isAuthor).toStrictEqual(true);
+// expect(a[1].isAuthor).toStrictEqual(true);
+// expect(a[2].isAuthor).toStrictEqual(true);
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+
+  people.forEach(person => {
+    person.isAuthor = true;
+  })
+
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -155,7 +166,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
